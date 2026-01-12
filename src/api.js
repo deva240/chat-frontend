db60e0c5976d145165468cca88140212ecdc4de2
Deva <deva@example.com>
2026-01-13 04:03:59 +0530

    Fix 401 by attaching JWT token to messages requests

--- src/api.js
+++ src/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const API_URL = "https://chat-backend-2nh2.onrender.com";
-
-// create axios instance
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://chat-backend-2nh2.onrender.com",
 });
 
-// attach token automatically
+// attach JWT automatically
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
